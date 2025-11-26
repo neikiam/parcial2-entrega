5843d6dc505cf9c398e4826aa0effada079627c8
brian guzman <neikiam500@gmail.com>
2025-11-26 17:49:56 -0300

    Actualización para la presentación en la entrega

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-aplicación hecha para un parcial
+
+Sistema de gestión de estudiantes con scraper de Google Scholar.
+
+En CMD:
+
+py -3.10 -m venv .venv
+.venv\Scripts\activate
+python --version
+python -m pip install --upgrade pip
+python -m pip install -U pip setuptools wheel
+python -m pip install -r requirements.txt
+
+Crear un .env basado en .env.example y luego
+
+python manage.py makemigrations
+python manage.py migrate
+python manage.py runserver
--- config/settings.py
+++ config/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -90,6 +91,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
