b7f798433b243e89adbcfb4f6ed35a971357b810
brian guzman <neikiam500@gmail.com>
2025-12-03 16:41:07 -0300

    Delete scraper directory

--- scraper/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import ScraperResult
-
-@admin.register(ScraperResult)
-class ScraperResultAdmin(admin.ModelAdmin):
-    list_display = ['titulo', 'palabra_clave', 'fecha_scraping']
-    list_filter = ['palabra_clave', 'fecha_scraping']
-    search_fields = ['titulo', 'palabra_clave']
--- scraper/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'scraper'
-
-urlpatterns = [
-    path('', views.scraper_view, name='scraper'),
-]
