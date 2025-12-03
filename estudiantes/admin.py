dec86a82036b790e01cc7fb1a7a8f3d2d9f419c3
brian guzman <neikiam500@gmail.com>
2025-12-03 16:40:59 -0300

    Delete estudiantes directory

--- estudiantes/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Alumno
-
-@admin.register(Alumno)
-class AlumnoAdmin(admin.ModelAdmin):
-    list_display = ['nombre', 'email', 'carrera', 'usuario', 'fecha_creacion']
-    list_filter = ['carrera', 'fecha_creacion']
-    search_fields = ['nombre', 'email']
